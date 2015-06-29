e58973c854c4105c2c590b0c748f011023f0ca45
thordur13 <thordur13@ru.is>
2015-06-29 10:45:01 +0000

    working on log.js
    Getting console to write out actionLink when clicked on... but gets
    loosing list when refreashing page

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -1,2 +1,15 @@
 ﻿console.log("Testing this log.js")
 console.log("And it worked :)")
+//This is working to write out to console... but not like it should be :(
+$(document).ready(function () {
+    $("a").click(console.log("Testing ActionLink"));
+});
+//Not working at all!
+//$(document).ready(function () {
+//    var objReWrite = {
+//        test: function () {
+//            $(console.log("testing actionLink()"));
+//        }
+//    };
+//    $("a").click($.proxy(objReWrite, "test"));
+//});
