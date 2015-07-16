2c92d87ebeaf2a0fb2aae67ae842f08f9ee7e831
thordur13 <thordur13@ru.is>
2015-07-16 13:46:20 +0000

    fikta í log.js skrá

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -1,9 +1,22 @@
 ﻿console.log("Testing this log.js")
 console.log("And it worked :)")
+
+var logArray = ["test"];
 //This is working to write out to console... but not like it should be :(
 $(document).ready(function () {
-    $("a").click(console.log("Testing ActionLink"));
+    $("a").click(function(){
+        logArray = this.URL;
+        (console.log("Testing ActionLink"));
+    })
 });
+console.log(logArray);
+
+//$(document).ready(function () {
+//    $("p").click(function () {
+//        $(this).fadeOut();
+//    });
+//});
+
 //Not working at all!
 //$(document).ready(function () {
 //    var objReWrite = {
