7c9fd60c9559cfd82cd1c295aa61545f401ab8cf
thordur13 <thordur13@ru.is>
2015-07-20 18:12:05 +0000

    snyrta log.js plugin aðeins til...

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -1,26 +1,16 @@
 ﻿;
 var logArray = [];
-var i = 0;
-//This is working to write out to console... but not staying there :(
-//$(document).ready(function () {
-//    $("a").click(function () {
-//        var urlValue = $(this).attr("href");
-//        var link = location.href;
-//        console.log(urlValue);
-//        console.log(link);
-//    })
-//});
-//console.log(logArray);
+var index = 0;
 
 (function ($) {
     $.fn.logActivity = function () {
         var urlValue;
-        $(this).click(urlValue = location.href);
-        //logArray[i] = urlValue;
-        $.each(urlValue, function (href) { logArray.push(href);})
-        //for (var i in logArray) {
-        //    logArray[i] = urlValue;
-        //}
+        var stopper = 20;
+        for (var j = 0; stopper > j; j++) {
+            $(this).click(urlValue = location.href);
+            logArray[index] = urlValue;
+            index++;
+        }
     };
 }(jQuery));
 $("a").logActivity();
@@ -28,15 +18,3 @@ $("a").logActivity();
 $.each(logArray, function (index, href) {
     console.log(index + ": " + href);
 })
-
-//(function ($) {
-//    $.fn.runArray = function () {
-//        $.each(logArray, function (index, href) {
-//            console.log(index + ": " + href);
-//        })
-//        //for (var j in logArray) {
-//        //    console.log(logArray[j]);
-//        //}
-//    };
-//}(jQuery));
-//$("div").runArray();
