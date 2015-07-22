40abd2fc1ac0e28ba0dd32600b5247a42c1bd203
thordur13 <thordur13@ru.is>
2015-07-22 19:47:47 +0000

    fikt sem virkar ekki á eftir að betrum bæta...

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -4,17 +4,29 @@ var urlStorage;
 var index = 0;
 
 (function ($) {
-    $.fn.logActivity = function () {
-        $(this).click(urlStorage = location.href);
-        logArray.push(urlStorage);
+    $.fn.extend({
+        logPlugin: function () {
+            return this.each(function(){
+                //this.click(alert(location.href))
+                console.log(location.href);
+                logArray.push(location.href)
+            
+            });
+        }
+    });
+    //$.fn.logActivity = function () {
+    //    $(this).click(urlStorage = location.href);
+    //    logArray.push(urlStorage);
         
-    };
+    //};
 }(jQuery));
-$("a").logActivity();
+//$("a").logActivity();
+$("a").logPlugin();
 
-console.log(urlStorage);
+//console.log(urlStorage);
+//logArray.push(urlStorage);
 console.log(logArray);
 
-//$.each(logArray, function (index, href) {
-//    console.log(index + ": " + href);
-//})
+$.each(logArray, function (index, href) {
+    console.log(index + ": " + href);
+})
