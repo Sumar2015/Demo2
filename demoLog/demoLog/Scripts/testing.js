bbecd905d6ef96596575d91368f7c9ba94e34149
Halldora Johannsdottir <hj02@ru.is>
2015-07-29 10:26:50 +0000

    fikt added

---
+++ demoLog/demoLog/Scripts/testing.js
@@ -0,0 +1,42 @@
+﻿;
+var arr = [];
+var loc;
+function log() {
+        $(this).click(loc = location.href);
+    //console.log("Link: " + urlStorage);
+        return (loc);
+};
+for(var i = 0; i < 5; i++)
+{
+    arr[i] = log();
+}
+
+for (var t = 0; t < 5; t++)
+{
+    console.log("Link: " + (arr[t]));
+}
+
+/*
+function multiply(x, y) {
+    var samt = x * y;
+    return (samt);
+}
+
+var arr = [];
+var a = 0;
+var b = 0;
+
+for (var i = 0; i < 10; i++)
+{
+    arr[i] = multiply(a, b);
+    a++;
+    b++;
+};
+
+for (var t = 0; t < 10; t++)
+{
+    console.log(arr[t]);
+};
+console.log(multiply(2, 2));
+console.log(multiply(4, 4));
+*/
