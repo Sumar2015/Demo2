7fd26e12f54024bbeaa9ccc0912f872844b26d3d
thordur13 <thordur13@ru.is>
2015-08-03 20:17:50 +0000

    inisializing logArray
    making some experiment with event.target outside main function and it
    works :) but not inside :(

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -1,29 +1,37 @@
-﻿;
-var logArray = [];
-var urlStorage;
-var index = 0;
-var date;
-var tag;
-var xCor, yCor;
+﻿$(document).ready(function () {
+    $("a").click(function (event) {
+        alert("tag: " + (event.target.tagName).toLowerCase());
+        alert("X coordinates: " + event.pageX + "\n" + "Y coordinates: " + event.pageY);
+    });
+});
 
 (function ($) {
     $.fn.logPlugin = function () {
-
+        //inisialize logArray if not inisialized before
+        if(localStorage.getItem("logArr") === null){
+           var logArray = {
+                urlStorage: "",
+                date: "",
+                tag: "",
+                xCor: "",
+                yCor: ""
+            }
+        };
         //onClickHref function that gets information about link clicked on
         (function onClickHref() {
-            var event;
-            $('a').click(function (event) {
-                logArray = {
+            $("a").click(function (event) {
+                return logArray = {
                     urlStorage : location.href,
                     date : Date(),
-                    /*tag : this.event.target.tagName,
-                    xCor : this.event.pageX,
-                    yCor : this.event.pageY,*/
+                    tag : event.target.tagName,
+                    xCor : event.pageX,
+                    yCor : event.pageY
                 }
                 console.log(logArray);
             }())
         })()
         //todo:
+        //setItem into localStorage
         //function for button, input including keydown
     
     };
