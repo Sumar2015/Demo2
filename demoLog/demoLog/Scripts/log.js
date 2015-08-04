96c4a6ce04302a90cbf210c06885762e03f88f63
thordur13 <thordur13@ru.is>
2015-08-04 20:40:55 +0000

    new

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -1,15 +1,28 @@
-﻿$(document).ready(function () {
+﻿$(function () {
     $("a").click(function (event) {
-        console.log("outside main function :(" + "\n" + "tag: " + (event.target.tagName).toLowerCase());
-        console.log("X coordinates: " + event.pageX + "\n" + "Y coordinates: " + event.pageY);
+        alert("1 "+event);
+        var tag = event.target.tagName.toLowerCase();
+        var date = Date();
+        var xCor = event.pageX;
+        var yCor = event.pageY;
+        alert("outside main function :(" + "\n" +
+            "tag: " + tag + "\n" +
+            "date: " + date + "\n" +
+            "url: " + url + "\n" +
+            "xCor: " + xCor + "\n" +
+            "yCor: " + yCor);
     });
+});
+$("a").click(function () {
+    logPugin(event);
 });
 
 (function ($) {
     $.fn.logPlugin = function () {
+        var logArray = [];
         //inisialize logArray if not inisialized before
         if(localStorage.getItem("logArr") === null){
-           var logArray = {
+           logArray = {
                 urlStorage: "",
                 date: "",
                 tag: "",
@@ -17,15 +30,18 @@
                 yCor: ""
             }
         };
+        
         //onClickHref function that gets information about link clicked on
         (function onClickHref() {
-            $("a").click(function (event) {
+            $("a").click(function () {
+                var tag = this;
+                alert("2 "+tag);
                 logArray = {
                     urlStorage: location.href,
                     date: Date(),
-                    /*tag : event.target.tagName,
-                    xCor : event.pageX,
-                    yCor : event.pageY*/
+                    //tag : (event.target.tagName).toLowerCase(),
+                    //xCor : (this.event.pageX),
+                    //yCor : event.pageY
                 }
                 console.log(logArray);
             }())
