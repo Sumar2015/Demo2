41f52815b9998460bf1281f0ae0763e952af2d67
Halldora Johannsdottir <hj02@ru.is>
2015-08-05 22:51:25 +0000

    onTab and event.type added

--- demoLog/demoLog/Scripts/log.js
+++ demoLog/demoLog/Scripts/log.js
@@ -11,6 +11,7 @@
             id: "",
             name: "",
             class: "",
+            event: "",
             xCor: "",
             yCor: ""
             
@@ -18,22 +19,32 @@
     };//todo: else condition to get old logArray from LS
 
     //onClick function getting information when clicking
-    $(function onClick () {
+    $(function onClick() {
         $("a, button, input, textarea, span, select").click(function (event) {
             dataInstall(event);
            console.log(currArray);
+        });
+    });
+
+    $(function onKeyPress() {
+        $("a, button, input, textarea, span, select").keypress(function (event) {
+            if (event.which == 9 || event.keycode == 9) {
+                dataInstall(event);
+                console.log(currArray);
+            }
         });
     });
     
     //installing data into currArray
     function dataInstall(ev) {
         currArray = {
-            urlStorage: "",
+            urlStorage: location.href,
             date: Date(),
             tag: event.target.tagName.toLowerCase(),
             id: event.target.id,
             name: event.target.name,
             class: event.target.className,
+            event: event.type,
             xCor: event.pageX,
             yCor: event.pageY
         };
