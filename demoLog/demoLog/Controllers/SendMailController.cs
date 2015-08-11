1c47af003bded96017b702a7c5a576e491f0cdb9
Halldora Johannsdottir <hj02@ru.is>
2015-08-11 23:11:45 +0000

    trying to connect submit with email

---
+++ demoLog/demoLog/Controllers/SendMailController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoLog.Controllers
+{
+    public class SendMailController : Controller
+    {
+        // GET: SendMail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+         /// <summary>
+        /// Send Mail with Gmail
+        /// </summary>
+        /// <param name="objModelMail">MailModel Object, keeps all properties</param>
+        /// <param name="fileUploader">Selected file data, example-filename,content,content type(file type- .txt,.png etc.),length etc.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(SendMailwithAttachment.Models.MailModel objModelMail, HttpPostedFileBase fileUploader)
+        {
+            if (ModelState.IsValid)
+            {
+                string from = "Your Gmail Id"; //example:- sourabh9303@gmail.com
+                using (MailMessage mail = new MailMessage(from, objModelMail.To))
+                {
+                    mail.Subject = objModelMail.Subject;
+                    mail.Body = objModelMail.Body;
+                    if (fileUploader != null)
+                    {
+                        string fileName = Path.GetFileName(fileUploader.FileName);
+                        mail.Attachments.Add(new Attachment(fileUploader.InputStream, fileName));
+                    }
+                    mail.IsBodyHtml = false;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential networkCredential = new NetworkCredential(from, "Your Gmail Password");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = networkCredential;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Sent";
+                    return View("Index", objModelMail);
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ demoLog/demoLog/Models/MailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demoLog.Models
+{
+    public class MailModel
+    {
+        public int ID { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
